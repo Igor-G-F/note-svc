b9743793871821aafa6710ded5bef4e39cb8a14d
Igor <igor@giffgaff.co.uk>
2019-04-09 15:36:04 +0100

    FEAT: first commit

---
+++ src/main/java/com/manus/noteark/notesvc/NoteSvcApplication.java
@@ -0,0 +1,13 @@
+package com.manus.noteark.notesvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manus/noteark/notesvc/NoteSvcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.manus.noteark.notesvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoteSvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

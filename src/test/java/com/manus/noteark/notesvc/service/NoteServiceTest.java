1a390678cceddbf167fbbd511c65985dfe7603b9
Igor <igor@giffgaff.co.uk>
2019-04-11 17:00:53 +0100

    DEBT: unit tests for NoteService

---
+++ src/test/java/com/manus/noteark/notesvc/service/NoteServiceTest.java
@@ -0,0 +1,136 @@
+package com.manus.noteark.notesvc.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.manus.noteark.notesvc.exception.NotFoundException;
+import com.manus.noteark.notesvc.pojo.Note;
+import com.manus.noteark.notesvc.repository.NoteRepository;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class NoteServiceTest {
+
+    @InjectMocks
+    private NoteService noteService;
+    @Mock
+    private NoteRepository noteRepositoryMock;
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Before 
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        noteService = new NoteService(noteRepositoryMock);
+    }
+
+    @Test
+    public void createNoteTest_ReturnsCreatedNote() {
+        Note requestNote = makeNote();
+        Note createdNote = makeNote();
+        createdNote.setId("1234");
+
+        when(noteRepositoryMock.save(requestNote))
+            .thenReturn(createdNote);
+
+        assertEquals(createdNote, noteService.createNote(requestNote));
+    }
+
+    @Test
+    public void updateNoteWithIdTest_NoteExists_ReturnsUpdatedNote() {
+        String updateTargetId = "1234";
+        Note requestNote = makeNote();
+        Note updatedNote = makeNote();
+        updatedNote.setId("1234");
+
+        when(noteRepositoryMock.updateById(updateTargetId, requestNote))
+            .thenReturn(Optional.of(updatedNote));
+
+        assertEquals(updatedNote, noteService.updateNoteWithId(updateTargetId, requestNote));
+    }
+
+    @Test
+    public void updateNoteWithIdTest_NoteDoesNotExist_ReturnsNewNote() {
+        String updateTargetId = "2345";
+        Note requestNote = makeNote();
+        Note updatedNote = makeNote();
+        updatedNote.setId("1234");
+
+        when(noteRepositoryMock.updateById(updateTargetId, requestNote))
+            .thenReturn(Optional.of(updatedNote));
+
+        assertEquals(updatedNote, noteService.updateNoteWithId(updateTargetId, requestNote));
+    }
+
+    @Test
+    public void getNoteByIdTest_NoteFound_ReturnsNote() {      
+        String noteId = "1234";
+        Note foundNote = makeNote();
+        foundNote.setId(noteId);
+
+        when(noteRepositoryMock.findById(noteId))
+            .thenReturn(Optional.of(foundNote));
+
+        assertEquals(foundNote, noteService.getNoteById(noteId));
+    }
+
+    @Test
+    public void getNoteByIdTest_NoteNotFound_ThrowsException() {      
+        String noteId = "1234";
+
+        when(noteRepositoryMock.findById(noteId))
+            .thenReturn(Optional.empty());
+
+        expectedException.expect(NotFoundException.class);
+        expectedException.expectMessage("Could not find \"note\" with id \"1234\"");
+
+        noteService.getNoteById(noteId);
+    }
+
+    @Test
+    public void getAllNotesForOwnerTest_HasNotes_ReturnsNotes() {
+        String noteOwner = "1234";
+        List<Note> notes = new ArrayList<>();
+        notes.add(makeNote());
+        notes.add(makeNote());
+
+        when(noteRepositoryMock.findByOwner(noteOwner))
+            .thenReturn(notes);
+
+        assertEquals(notes, noteService.getAllNotesForOwner(noteOwner));
+    }
+
+    @Test
+    public void getAllNotesForOwnerTest_HasNoNotes_ReturnsEmptyList() {
+        String noteOwner = "1234";
+        List<Note> notes = new ArrayList<>();
+
+        when(noteRepositoryMock.findByOwner(noteOwner))
+            .thenReturn(notes);
+
+        assertEquals(notes, noteService.getAllNotesForOwner(noteOwner));
+    }
+
+    private Note makeNote() {
+        Note result = new Note();
+        result.setId(null);
+        result.setOwner("5678");
+        result.setTitle("Test title");
+        result.setContent("Test content");
+        return result;
+    }
+
+}

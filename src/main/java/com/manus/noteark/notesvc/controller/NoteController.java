bc27e96a48dfeea68471f29d0aa811e8b1b2c842
Igor <igor@giffgaff.co.uk>
2019-04-12 16:38:00 +0100

    FEAT: added swagger

--- src/main/java/com/manus/noteark/notesvc/controller/NoteController.java
+++ src/main/java/com/manus/noteark/notesvc/controller/NoteController.java
@@ -12,10 +12,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/note")
 public class NoteController {
 
     private final NoteService noteService;
@@ -24,28 +26,22 @@ public class NoteController {
         this.noteService = noteService;
     }
 
-    @PostMapping("/note")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Note createNote(@RequestBody Note note) {
         return noteService.createNote(note);
     }
 
-    @PutMapping("/note/{noteId}")
+    @PutMapping("/{noteId}")
     public Note updateNoteWithId(@PathVariable String noteId, @RequestBody Note note, HttpServletResponse response) {
         Note result = noteService.updateNoteWithId(noteId, note);
         if (!result.getId().equals(noteId)) response.setStatus(HttpServletResponse.SC_CREATED);
         return result;
     }
 
-    @GetMapping("/note/{noteId}")
+    @GetMapping("/{noteId}")
     public Note getNoteById(@PathVariable String noteId) {
         return noteService.getNoteById(noteId);
     }
-
-    @GetMapping("/notes/{owner}")
-    public List<Note> getAllNotesForOwner(@PathVariable String owner) {
-        return noteService.getAllNotesForOwner(owner);
-    }
-
-
+    
 }
---
+++ src/main/java/com/manus/noteark/notesvc/controller/OwnerController.java
@@ -0,0 +1,27 @@
+package com.manus.noteark.notesvc.controller;
+
+import java.util.List;
+
+import com.manus.noteark.notesvc.pojo.Note;
+import com.manus.noteark.notesvc.service.NoteService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/owner")
+public class OwnerController {
+
+    private final NoteService noteService;
+
+    public OwnerController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @GetMapping("/{ownerId}/notes")
+    public List<Note> getAllNotesForOwner(@PathVariable String ownerId) {
+        return noteService.getAllNotesForOwner(ownerId);
+    }
+
+}

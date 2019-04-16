6218e948a6eaf95f87b01c519eff2b83103c566e
Igor <igor@giffgaff.co.uk>
2019-04-16 14:47:45 +0100

    FEAT: swagger config

--- src/main/java/com/manus/noteark/notesvc/config/SwaggerConfig.java
+++ src/main/java/com/manus/noteark/notesvc/config/SwaggerConfig.java
@@ -2,16 +2,21 @@ package com.manus.noteark.notesvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import io.swagger.models.Contact;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {     
+public class SwaggerConfig extends WebMvcConfigurationSupport {     
                                    
     @Bean
     public Docket api() { 
@@ -19,6 +24,24 @@ public class SwaggerConfig {
           .select()                                  
           .apis(RequestHandlerSelectors.basePackage("com.manus.noteark.notesvc.controller"))              
           .paths(PathSelectors.any())                          
-          .build();                                           
+          .build()
+          .apiInfo(metaData());
     }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+            .title("Note Ark - note-svc")
+            .description("\"Note management service, exposes storage, retrieval and updating of notes.\"")
+            .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
 }

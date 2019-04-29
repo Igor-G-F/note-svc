72e807ffd506fb8bb198a96d5cbeba83f65a5d20
Igor <igor@giffgaff.co.uk>
2019-04-29 15:13:24 +0100

    FEAT: working env configs

--- src/main/java/com/manus/noteark/notesvc/config/MongoConfig.java
+++ src/main/java/com/manus/noteark/notesvc/config/MongoConfig.java
@@ -1,5 +1,6 @@
 package com.manus.noteark.notesvc.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -9,22 +10,23 @@ import com.mongodb.MongoClient;
 @Configuration
 @EnableMongoRepositories(basePackages = "com.manus.noteark.notesvc.repository")
 public class MongoConfig extends AbstractMongoConfiguration {
+
+    @Value("${mongodb.host}")
+    private String mongoHost;
+
+    @Value("${mongodb.port}")
+    private int mongoPort;
+
     @Override
     protected String getDatabaseName() {
         return "noteark";
     }
-  
+
     @Override
     public MongoClient mongoClient() {
-        return new MongoClient("localhost", 27017);
+        return new MongoClient(mongoHost, mongoPort);
     }
 
-    // CONFIG FOR DOCKER
-    // @Override
-    // public MongoClient mongoClient() {
-    //     return new MongoClient("mongodb", 27017);
-    // }
-  
     @Override
     protected String getMappingBasePackage() {
         return "com.manus.noteark.notesvc";
